e0ef2cf3b2ae74cf2fcde39f0ae91cf55477a76f
Victoria Dunham <vkurzweg@gmail.com>
2017-04-15 18:04:58 -0700

    add and configure material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "../stylesheets/main.scss";
+import { connect } from "react-redux";
+import { ProgressBar } from "react-bootstrap";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 
 // app component
 export default class App extends React.Component {
+
+  componentWillMount() {
+    injectTapEventPlugin();
+  }
+
   // render
   render() {
     return (
-      <div className="container">
-        {this.props.children}
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <div className="container">
+          {this.props.children}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
